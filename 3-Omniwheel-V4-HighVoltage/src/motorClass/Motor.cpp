26a80e6152cb167b2f6b8198b451466d3071ce54
debyyyy <155076112+debyyyy@users.noreply.github.com>
2024-11-19 22:32:58 +0100

    second constructeur + modif setSpeed()

--- 3-Omniwheel-V4-HighVoltage/src/motorClass/Motor.cpp
+++ 3-Omniwheel-V4-HighVoltage/src/motorClass/Motor.cpp
@@ -11,6 +11,16 @@ Motor::Motor(int RPWM, int LPWM, int SpeedLimit)
   pinMode(this->m_LPWM, OUTPUT);
 }
 
+// le constructeur a appelé si pas envie de définir la vitesse max(auto 255)
+Motor::Motor(int RPWM, int LPWM)
+{
+  this->m_speedLimit = 255;
+  this->m_RPWM = RPWM;
+  this->m_LPWM = LPWM;
+  pinMode(this->m_RPWM, OUTPUT);
+  pinMode(this->m_LPWM, OUTPUT);
+}
+
 Motor::~Motor()
 {
   setSpeed(0);
@@ -31,15 +41,20 @@ void Motor::setRotation(rotation Rotation)
 // Speed entre 0 et 255, le sens de rotation est donc géré via une méthode externe
 int Motor::setSpeed(int speed)
 {
-  if(this->m_rotation == rotation::RELEASE)
+  switch (this->m_rotation)
   {
+  case rotation::RELEASE :
+    analogWrite(this->m_RPWM, 0);
+    analogWrite(this->m_LPWM, 0);
     return 0;
-  } else if(this->m_rotation == rotation::FORWARD){
-     analogWrite(this->m_RPWM, speed);
-     analogWrite(this->m_LPWM, 0);
-    } else if (this-> m_rotation == rotation::BACKWARD){
-      analogWrite(this-> m_LPWM, speed);
-      analogWrite(this->m_RPWM, 0);
-    }
-
+  case rotation::FORWARD :
+    analogWrite(this->m_RPWM, speed);
+    analogWrite(this->m_LPWM, 0);
+    return 0;
+  case rotation::BACKWARD :
+    analogWrite(this->m_RPWM, 0);
+    analogWrite(this->m_LPWM, speed);
+    return 0;
+  }
+  return -1; // pas bon paramètre, erreur
 }
