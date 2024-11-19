ba49bf7c86b5d7d968f8ed30561cca1efffba73b
Klugame <ligueketoniejudo@gmail.com>
2024-11-19 17:48:24 +0100

    Motor class and Motor Lib.h
    Co-Authored-By: debyyyy <155076112+debyyyy@users.noreply.github.com>

--- 3-Omniwheel-V4-HighVoltage/src/main.cpp
+++ 3-Omniwheel-V4-HighVoltage/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <PS2X_lib.h>
 
 
 void setup() {
---
+++ 3-Omniwheel-V4-HighVoltage/src/motorLib/motorLib.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <Arduino.h>
+#include <PS2X_lib.h>
+
+// Déclaration des  fonction
+void motorMove();
+
+// Déclaration des constante
+
+// Déclaration des variable
+
+// Déclaration pin pwm des moteur en structure
+
+// 1
+struct m1
+{
+    /* data */
+    int Lpwm = 2;
+    int Rpwm = 3;
+};
+
+// 2
+struct m2
+{
+    /* data */
+    int Lpwm = 4;
+    int Rpwm = 5;
+};
+
+// 3
+struct m3
+{
+    /* data */
+    int Lpwm = 6;
+    int Rpwm = 7;
+};
+
+struct m4
+{
+    /* data */
+    int Lpwm = 8;
+    int Rpwm = 9;
+};
+
+
+
+
